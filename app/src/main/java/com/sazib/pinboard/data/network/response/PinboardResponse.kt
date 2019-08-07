a7d6090364089744b7454d3489d86b3b27ca250a
sazib <sazibislam1@gmail.com>
2019-08-07 06:24:21 +0600

    response working

--- app/src/main/java/com/sazib/pinboard/data/network/response/PinboardResponse.kt
+++ app/src/main/java/com/sazib/pinboard/data/network/response/PinboardResponse.kt
@@ -6,27 +6,27 @@ import com.google.gson.annotations.SerializedName
 data class PinboardResponse internal constructor(
   @SerializedName("id") @Expose var id: String? = null,
   @SerializedName("created_at") @Expose var created_at: String? = null,
-  @SerializedName("width") @Expose var width: Int? = null,
-  @SerializedName("height") @Expose var height: Int? = null,
+  @SerializedName("width") @Expose var width: Double? = null,
+  @SerializedName("height") @Expose var height: Double? = null,
   @SerializedName("color") @Expose var color: String? = null,
   @SerializedName("likes") @Expose var likes: Int? = null,
   @SerializedName("liked_by_user") @Expose var liked_by_user: Boolean? = null,
-  @SerializedName("user") @Expose var userList: ArrayList<UserData>? = null,
+  @SerializedName("user") @Expose var userList: UserData? = null,
   @SerializedName(
       "current_user_collections"
   ) @Expose var categories: ArrayList<Categories>? = null,
-  @SerializedName("urls") @Expose var urlsList: ArrayList<UrlsList>? = null,
-  @SerializedName("links") @Expose var linksList: ArrayList<LinksList>? = null
+  @SerializedName("urls") @Expose var urlsList: UrlsList? = null,
+  @SerializedName("links") @Expose var linksList: LinksList? = null
 ) {
   data class UserData internal constructor(
     @SerializedName("id") @Expose var id: String? = null,
     @SerializedName("username") @Expose var username: String? = null,
     @SerializedName("name") @Expose var name: String? = null,
-    @SerializedName("profile_image") @Expose var profile_image: ArrayList<ProfileImage>? = null,
-    @SerializedName("links") @Expose var links: ArrayList<ProfileLinks>? = null
+    @SerializedName("profile_image") @Expose var profile_image: ProfileImage? = null,
+    @SerializedName("links") @Expose var links: ProfileLinks? = null
   )
 
-  data class UrlsList(
+  data class UrlsList internal constructor(
     @SerializedName("raw") @Expose var raw: String? = null,
     @SerializedName("full") @Expose var full: String? = null,
     @SerializedName("regular") @Expose var regular: String? = null,
@@ -34,26 +34,26 @@ data class PinboardResponse internal constructor(
     @SerializedName("thumb") @Expose var thumb: String? = null
   )
 
-  data class LinksList(
+  data class LinksList internal constructor(
     @SerializedName("self") @Expose var id: String? = null,
     @SerializedName("html") @Expose var html: String? = null,
     @SerializedName("download") @Expose var download: String? = null
   )
 
-  data class ProfileImage(
+  data class ProfileImage internal constructor(
     @SerializedName("small") @Expose var small: String? = null,
     @SerializedName("medium") @Expose var medium: String? = null,
     @SerializedName("large") @Expose var large: String? = null
   )
 
-  data class ProfileLinks(
+  data class ProfileLinks internal constructor(
     @SerializedName("self") @Expose var self: String? = null,
     @SerializedName("html") @Expose var html: String? = null,
     @SerializedName("photos") @Expose var photos: String? = null,
     @SerializedName("likes") @Expose var likes: String? = null
   )
 
-  data class Categories(
+  data class Categories internal constructor(
     @SerializedName("id") @Expose var id: String? = null,
     @SerializedName("title") @Expose var title: String? = null,
     @SerializedName("photo_count") @Expose var photo_count: Int? = null,
@@ -62,7 +62,7 @@ data class PinboardResponse internal constructor(
     ) @Expose var currentUserCollectionsLinks: ArrayList<CollectionsLinks>? = null
   )
 
-  data class CollectionsLinks(
+  data class CollectionsLinks internal constructor(
     @SerializedName("self") @Expose var self: String? = null,
     @SerializedName("photos") @Expose var photos: String? = null
   )
