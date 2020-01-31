e6bf2d3c97c7fadcbd7acf9e422b47165f990ac0
sazib <sazibislam1@gmail.com>
2020-01-31 12:19:22 +0600

    updating

--- app/src/main/java/com/sazib/pinboard/utils/CommonUtil.kt
+++ app/src/main/java/com/sazib/pinboard/utils/CommonUtil.kt
@@ -2,15 +2,13 @@ package com.sazib.pinboard.utils
 
 import android.app.Dialog
 import android.content.Context
-import android.graphics.Color
-import androidx.core.graphics.drawable.toDrawable
 import com.sazib.pinboard.R
 
 object CommonUtil {
   fun showLoadingDialog(context: Context): Dialog {
     val dialog = Dialog(context)
     dialog.show()
-    dialog.window?.setBackgroundDrawable(Color.TRANSPARENT.toDrawable())
+    dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
     dialog.setContentView(R.layout.progress_dialog)
     dialog.setCancelable(false)
     dialog.setCanceledOnTouchOutside(false)
